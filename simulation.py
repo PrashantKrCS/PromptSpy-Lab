43d96e683bd05bb5a49c5675c1baf30ffd612444
PrashantKrCS <35539577+PrashantKrCS@users.noreply.github.com>
2026-06-22 01:13:23 +0530

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,20 +1,22 @@
-import random 
+import random
 
-from templates import PRETEXTS 
-from templates import EMAIL_TEMPLATE 
+from templates import PRETEXTS
+from templates import EMAIL_TEMPLATE
 
-class CampaignSimulator: 
-    def generate_campaign(self, persona): 
-        
-        theme = random.choice(PRETEXTS) 
-        
-        email = EMAIL_TEMPLATE.format( 
-            theme=theme, 
-            name=persona["name"], 
-            interest=persona["interest"] 
-         ) 
-         
-         return { 
-            "theme": theme, 
-            "email": email 
-         }
+
+class CampaignSimulator:
+
+    def generate_campaign(self, persona):
+
+        theme = random.choice(PRETEXTS)
+
+        email = EMAIL_TEMPLATE.format(
+            theme=theme,
+            name=persona["name"],
+            interest=persona["interest"]
+        )
+
+        return {
+            "theme": theme,
+            "email": email
+        }
