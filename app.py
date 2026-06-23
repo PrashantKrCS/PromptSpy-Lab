6e2f4d3a8e25df30f98b28e6129f2dd47fee5bc4
PrashantKrCS <35539577+PrashantKrCS@users.noreply.github.com>
2026-06-23 14:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,15 +55,25 @@ def payload_extension():
 def payload_mfa():
     return render_template("payload_mfa.html")
 
+#@app.route("/poison-memory")
+#def poison_memory():
+#
+#    memory = simulate_memory_poisoning()
+#
+#    return render_template(
+#        "memory_poisoned.html",
+#        memory=memory
+#    )
+
 @app.route("/poison-memory")
 def poison_memory():
 
-    memory = simulate_memory_poisoning()
+    simulate_memory_poisoning()
 
-    return render_template(
-        "memory_poisoned.html",
-        memory=memory
-    )
+    return """
+    <h2>Memory Poisoned</h2>
+    <a href='/'>Return To Dashboard</a>
+    """
 
 @app.route("/open")
 def open_email():
